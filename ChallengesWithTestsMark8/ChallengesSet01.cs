76b232cdedb61de28aa7767a24166111f372ca23
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-07-21 21:00:18 -0400

    week one challenge complete

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Win32.SafeHandles;
 
 namespace ChallengesWithTestsMark8
 {
@@ -18,32 +20,37 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
-        public int Add(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
-
+      public int Add(int number1, int number2)
+{
+    return number1 + number2;
+}
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+                return Math.Min(number1, number2);
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
+
+        
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+        
+    if (string.IsNullOrEmpty(nameOfPerson))
+        return "Hello!";
+    return $"Hello, {nameOfPerson}!";
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
