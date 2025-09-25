a20d95648764552cddc0423201ac043869e47c52
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-09-25 14:29:37 -0400

    week 4 resubmit , along with 3

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -9,37 +9,37 @@ namespace ChallengesWithTestsMark8
     {
         public ChallengesSet04()
         {
-        }
 
-        public int AddEvenSubtractOdd(int[] numbers)
-        {
-            var sum = 0;
 
-            foreach (var num in numbers)
+            public int AddEvenSubtractOdd(int[] numbers)
             {
-                if (num % 2 == 0)
+                var sum = 0;
+
+                foreach (var num in numbers)
                 {
-                    sum += num;
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                    }
+
+                    if (num % 2 != 0)
+                    {
+                        sum -= num;
+                    }
                 }
 
-                if (num % 2 != 0)
-                {
-                    sum -= num;
-                }
+                //return sum;
+                return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
             }
 
-            //return sum;
-            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
-        }
+            public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+            {
+                var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+                return list.Min();
 
-        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
-        {
-            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
-            return list.Min();
+            }
 
-        }
-
-        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+            public int GetSmallestNumber(int number1, int number2, int number3, int number4)
             {
                 var smallest = number1;
                 var list = new List<int> { number1, number2, number3, number4 };
@@ -52,98 +52,102 @@ namespace ChallengesWithTestsMark8
                         smallest = list[i];
                     }
                 }
+
                 return smallest;
             }
 
             public void ChangeBusinessNameTo_TrueCoders(Business biz)
-        {
-            biz.Name = "TrueCoders";
-        }
-
-        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
-        {
-            return
-                (sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 >
-                    sideLength2 && sideLength2 + sideLength3 > sideLength1);
-                
-        }
-
-        public bool IsStringANumber(string input)
-        {
-            var isNumber = double.TryParse(input, out double number);
-            return isNumber;
-        }
-
-        public bool MajorityOfElementsInArrayAreNull(object[] objs)
-        {
-            var amount = objs.Length;
-            var majority = (amount / 2) + 1;
-            var count = 0;
-            
-            foreach (var obj in objs)
             {
-                if (obj == null)
+                biz.Name = "TrueCoders";
+            }
+
+            public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+            {
+                return
+                    (sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 >
+                        sideLength2 && sideLength2 + sideLength3 > sideLength1);
+
+            }
+
+            public bool IsStringANumber(string input)
+            {
+                var isNumber = double.TryParse(input, out double number);
+                return isNumber;
+            }
+
+            public bool MajorityOfElementsInArrayAreNull(object[] objs)
+            {
+                var amount = objs.Length;
+                var majority = (amount / 2) + 1;
+                var count = 0;
+
+                foreach (var obj in objs)
                 {
-                    count++;
+                    if (obj == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count >= majority)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
-            if(count >= majority)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
 
-        public double AverageEvens(int[] numbers)
-        {
-            double sum = 0;
-            double count = 0;
-            
-            if (numbers == null)
+            public double AverageEvens(int[] numbers)
             {
-                return 0;
-            }
-            
-            foreach (var num in numbers)
-            {
-                if (num % 2 == 0)
+                double sum = 0;
+                double count = 0;
+
+                if (numbers == null)
                 {
-                    sum += num;
-                    count++;
+                    return 0;
                 }
-            }
-            
-            // if (count == 0)
-            // {
-            //     return sum / count;
-            // }
-            // else
-            // {
-            //     return 0;
-            // }
 
-            if ( numbers == null || numbers.Length == 0 ) 
+                foreach (var num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                        count++;
+                    }
+                }
+
+                // if (count == 0)
+                // {
+                //     return sum / count;
+                // }
+                // else
+                // {
+                //     return 0;
+                // }
+
+                if (numbers == null || numbers.Length == 0)
+                {
+                    return 0;
+                }
+
+                var n = numbers.Where(num => num % 2 == 0);
+                if (!n.Any())
+                    return 0;
+
+                return n.Average();
+            }
+
+            public int Factorial(int number)
             {
-                return 0;
-            }
-            
-            var n = numbers.Where(num => num % 2 == 0);
-            if (!n.Any())
-                return 0;
-            
-            return n.Average();
-        }
+                if (number == 0)
+                {
+                    return 1;
+                }
 
-    public int Factorial(int number)
-    {
-        if (number == 0)
-        {
-            return 1;
+                return number * Factorial(number - 1);
+            }
         }
-        return number * Factorial(number - 1);
-    }
     }
 }
